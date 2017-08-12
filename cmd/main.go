ffe80086c8b746088a7bfbaa6d22961319ab0a1d
Gabriel SULLICE <gabriel@sullice.com>
2017-08-12 16:40:39 -0600

    Get custom headers working. Move vendor dir

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"github.com/gabesullice/pdfaf/lib/core"
+	"context"
+	"encoding/json"
+	"io"
 	"os"
+	"os/exec"
+	"time"
+
+	"github.com/gabesullice/pdfaf/lib/core"
 )
 
 func main() {
+	runChrome(context.TODO())
+
 	file, err := os.Create("./output.pdf")
 	defer file.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	r, err := core.RequestToPDF(core.URL(os.Args[1]))
+	headerMap := make(map[string]string)
+	if err := json.Unmarshal([]byte(os.Args[2]), &headerMap); err != nil {
+		panic(err)
+	}
+	r, err := core.RequestToPDF(
+		core.URL(os.Args[1]),
+		core.Headers(headerMap),
+	)
+
 	if err != nil {
 		panic(err)
 	}
@@ -20,4 +36,14 @@ func main() {
 	if _, err = io.Copy(file, r); err != nil {
 		panic(err)
 	}
+}
+
+func runChrome(ctx context.Context) {
+	go func() {
+		cmd := exec.CommandContext(ctx, "chromium", "--headless", "--remote-debugging-port=9222", "--disable-gpu")
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+	}()
+	time.Sleep(500 * time.Millisecond)
 }
--- lib/core/core.go
+++ lib/core/core.go
@@ -3,16 +3,19 @@ package core
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"io"
 
 	"github.com/mafredri/cdp"
 	"github.com/mafredri/cdp/devtool"
+	"github.com/mafredri/cdp/protocol/network"
 	"github.com/mafredri/cdp/protocol/page"
 	"github.com/mafredri/cdp/rpcc"
 )
 
 type Options struct {
-	URL string
+	URL     string
+	Headers []byte
 }
 
 type OptionFunc func(Options) Options
@@ -20,6 +23,13 @@ type OptionFunc func(Options) Options
 func URL(url string) OptionFunc {
 	return func(o Options) Options {
 		o.URL = url
+		return o
+	}
+}
+
+func Headers(headers map[string]string) OptionFunc {
+	return func(o Options) Options {
+		o.Headers, _ = json.Marshal(headers)
 		return o
 	}
 }
@@ -58,7 +68,18 @@ func RequestToPDF(ops ...OptionFunc) (io.Reader, error) {
 	// Enable events on the Page domain, it's often preferrable to create
 	// event clients before enabling events so that we don't miss any.
 	if err = c.Page.Enable(ctx); err != nil {
-		return err
+		return nil, err
+	}
+
+	if opts.Headers != nil {
+		// Enable events on the Network domain.
+		if err = c.Network.Enable(ctx, network.NewEnableArgs()); err != nil {
+			return nil, err
+		}
+
+		if err := c.Network.SetExtraHTTPHeaders(ctx, network.NewSetExtraHTTPHeadersArgs(opts.Headers)); err != nil {
+			return nil, err
+		}
 	}
 
 	navArgs := page.NewNavigateArgs(opts.URL)
@@ -68,7 +89,7 @@ func RequestToPDF(ops ...OptionFunc) (io.Reader, error) {
 
 	// Wait until we have a DOMContentEventFired event.
 	if _, err = loaded.Recv(); err != nil {
-		return err
+		return nil, err
 	}
 
 	reply, err := c.Page.PrintToPDF(ctx, &page.PrintToPDFArgs{})
